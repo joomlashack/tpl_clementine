d7f1b3fa57255509f3b1523bfb5b1c1c29b7dd2e
jmarkos <markos@joomlashack.com>
2015-04-04 01:59:00 -0600

    adding icons of home page

---
+++ code/html/mod_artcles_news/image_top.php
@@ -0,0 +1,20 @@
+<?php
+// Wright v.3 Override: Joomla 2.5.28 and 3.3
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_articles_news
+ * @copyright	Copyright (C) 2005 - 2015 Open Source Matters, Inc. 
+ * 				All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+	$wrightNewsEnableIcons = false;
+	$wrightEnableLinkContent = true;
+	$wrightImageFirst = true;
+	$wrightMaxColumns = 6;
+
+include(Overrider::getOverride('mod_articles_news','horizontal'));
+?>
